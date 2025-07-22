69a2e040d9ceac1202f6eaba7bfef5227ed53bec
Messias Junior <messias.rocha1987@gmail.com>
2025-07-22 16:33:11 -0300

    Funcao Retorno Motorsita

---
+++ 6_funcao_motorsitas.js
@@ -0,0 +1,16 @@
+
+let casa = ["Ernesto", "Costa", "Ladislau", "Queiroz", "Soldado", "Eliezio", "Rodrigo"]
+let terceirizado = ["Micael", "Lucas", "James","Alex", "Venicio", "Edelson", "jocielton"]
+
+//Função para retornar tipo de motorista
+function retornaMotorista(motorista) {
+  if (casa.includes(motorista)) {
+    return console.log("O Motorusta é da casa")
+  } else if (terceirizado.includes(motorista)) {
+    return console.log("O Motorista é Terceirizado")
+  } else {
+    return console.log ("O Motorista não trabalha na Empresa")
+  }
+}
+
+  retornaMotorista("Lucas")
