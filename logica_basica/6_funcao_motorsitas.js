a80970bbbff055a7a43855a5b39742bad8168afa
Messias Junior <messias.rocha1987@gmail.com>
2025-07-22 16:58:43 -0300

    Ajuste de pasta

--- logica_basica/6_funcao_motorsitas.js
+++ logica_basica/6_funcao_motorsitas.js
@@ -2,10 +2,10 @@
 let casa = ["Ernesto", "Costa", "Ladislau", "Queiroz", "Soldado", "Eliezio", "Rodrigo"]
 let terceirizado = ["Micael", "Lucas", "James","Alex", "Venicio", "Edelson", "jocielton"]
 
-//Função para retornar tipo de motorista
+//Função para retornar tipo de motorista 
 function retornaMotorista(motorista) {
   if (casa.includes(motorista)) {
-    return console.log("O Motorusta é da casa")
+    return console.log("O Motorista é da casa")
   } else if (terceirizado.includes(motorista)) {
     return console.log("O Motorista é Terceirizado")
   } else {
