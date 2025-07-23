a7fb87aa744d488ad38aeb0f88b71fd014758b60
Messias Junior <messias.rocha1987@gmail.com>
2025-07-23 15:27:15 -0300

    Get Cargo Value

--- logica_avancada/5_getDriversBetween.js
+++ logica_avancada/5_getDriversBetween.js
@@ -4,7 +4,13 @@ const motoristas = require('../data/motoristas.js')
 
 
 function getDriversBetween() {
+  const motoristasCargoValor = motoristas.filter(m => m.cargoValue >= "200" && m.cargoValue <= "300")
+  if (motoristasCargoValor.length === 0) {
+    return console.log("Menhum motorista tem cargo com esse valor")
+  }
+  return console.log(motoristasCargoValor)
+}
   // Filtrar motoristas com cargoValue entre 200 e 300
   // Retornar os motoristas filtrados
-}
+getDriversBetween()
 
