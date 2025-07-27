6753f9b33abd40dcaa857fe417a379cce4b6e9bc
RicardoOpus <ricardoopus125@gmail.com>
2025-07-27 18:10:36 -0300

    Add initial implementation of getDriversCargoValues function

---
+++ logica_avancada/10_getDriversCargoValues.js
@@ -0,0 +1,9 @@
+// Vamos criar uma função que retorna os VALORES das cargas dos motoristas
+// e o nome de cada motorista
+const motoristas = require("../data/motoristas.js");
+
+const getDriversCargoValues = () => {
+  // Implementa aqui a lógica para obter os valores das cargas dos motoristas
+};
+
+getDriversCargoValues()
