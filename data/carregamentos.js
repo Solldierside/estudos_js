659d15b7bc867529d781e531948665c4ba198d8d
RicardoOpus <ricardoopus125@gmail.com>
2025-07-27 22:33:54 -0300

    Add carregamentos data and initial implementation of getCargosByDriverName function

---
+++ data/carregamentos.js
@@ -0,0 +1,88 @@
+const carregamentos = [
+  {
+    id: 1,
+    motoristaId: 1,
+    numeroDoCarregamento: "152318",
+    cargoValue: 100.0,
+  },
+  {
+    id: 2,
+    motoristaId: 2,
+    numeroDoCarregamento: "152319",
+    cargoValue: 200.0,
+  },
+  {
+    id: 3,
+    motoristaId: 1,
+    numeroDoCarregamento: "152320",
+    cargoValue: 150.0,
+  },
+  {
+    id: 4,
+    motoristaId: 3,
+    numeroDoCarregamento: "152321",
+    cargoValue: 300.0,
+  },
+  {
+    id: 5,
+    motoristaId: 4,
+    numeroDoCarregamento: "152322",
+    cargoValue: 250.0,
+  },
+  {
+    id: 6,
+    motoristaId: 5,
+    numeroDoCarregamento: "152323",
+    cargoValue: 400.0,
+  },
+  {
+    id: 7,
+    motoristaId: 5,
+    numeroDoCarregamento: "152324",
+    cargoValue: 350.0,
+  },
+  {
+    id: 8,
+    motoristaId: 5,
+    numeroDoCarregamento: "152325",
+    cargoValue: 450.0,
+  },
+  {
+    id: 9,
+    motoristaId: 8,
+    numeroDoCarregamento: "152326",
+    cargoValue: 500.0,
+  },
+  {
+    id: 10,
+    motoristaId: 9,
+    numeroDoCarregamento: "152327",
+    cargoValue: 550.0,
+  },
+  {
+    id: 11,
+    motoristaId: 10,
+    numeroDoCarregamento: "152328",
+    cargoValue: 600.0,
+  },
+  {
+    id: 12,
+    motoristaId: 11,
+    numeroDoCarregamento: "152329",
+    cargoValue: 650.0,
+  },
+  {
+    id: 13,
+    motoristaId: 12,
+    numeroDoCarregamento: "152330",
+    cargoValue: 700.0,
+  },
+  {
+    id: 14,
+    motoristaId: 13,
+    numeroDoCarregamento: "152331",
+    cargoValue: 750.0,
+  },
+];
+
+module.exports = carregamentos;
---
+++ logica_avancada/11_getCargosByDriverName.js
@@ -0,0 +1,11 @@
+// Vamos implementar uma função que recebe o Nome do Motorista e retorna todos os Carregamentos
+
+
+
+
+const getCargosByDriverName = () => {
+  // Implementa aqui a lógica para obter os carregamentos do motorista pelo nome
+};
+
+// Exemplo de uso da função
+getCargosByDriverName("Soldado");
